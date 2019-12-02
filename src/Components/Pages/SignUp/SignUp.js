311f417da48358ce0a6c43bae7e82649440f6d25
Adeola <adeola@Adeolas-iMac.local>
2019-12-02 11:21:34 +0100

    Store lastname,email, meternumber,password & disco

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -7,6 +7,11 @@ import TopMenu from '../../TopMenu/TopMenu';
 class SignUp extends React.Component {
     state = {
         firstName: '',
+        lastName: '',
+        emailAddress: '',
+        meterNumber: '',
+        passWord: '',
+        disco: '',
         redirect: false
     }
 
@@ -26,7 +31,14 @@ class SignUp extends React.Component {
         this.setState({
           redirect: true
         })
-        localStorage.setItem("firstName",this.state.firstName)
+        localStorage.setItem("firstName",this.state.firstName);
+        localStorage.setItem("lastName",this.state.lastName);
+        localStorage.setItem("emailAddress",this.state.emailAddress);
+        localStorage.setItem("meterNumber",this.state.meterNumber);
+        localStorage.setItem("passWord",this.state.passWord);
+        localStorage.setItem("disco",this.state.disco);
+
+        
       }
 
       renderRedirect = () => {
@@ -55,18 +67,17 @@ class SignUp extends React.Component {
                     <hr />
                 </div>
                 <form className="reg_form" onSubmit={this.handleFormSubmit}>
-                    {/* <input type="number" value={phoneNumber} onChange={this.handlePhone} placeholder="Enter your Phone Number" required /> */}
-                    <input type="number" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Enter your Phone Number" autoComplete="off" required />
-                    {/* <input type="number" name="Phone Number" value={this.state.phoneNumber} onChange={this.setPhone} maxlength="11" placeholder="Enter your Phone Number" autoComplete="on" autofocus required /> */}
-                    <input type="text" name="firstName" value={this.state.firstName} onChange={ this.handleChange} placeholder="Enter your First Name" required />
-                    <input type="text" name="LastName" placeholder="Enter your Last Name" autoComplete="on" required />
-                    <input type="email" name="EmailAddress" placeholder="Enter your Email Address." autoComplete="on" required />
-                    <input type="text" name="Meter Number" max="9" placeholder="Enter your Meter Number" autoComplete="on" required />
-                    <input type="password" name="Password" placeholder="Enter your Password" required />
-                    <select id="disco" name="disco" required>
-                        <option value="australia">Select disco</option>
-                        <option value="canada">Eko Electric</option>
-                        <option value="usa">ADEC</option>
+                   
+                    <input type="number" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Enter your phone number" autoComplete="off" required />
+                    <input type="text" name="firstName" value={this.state.firstName} onChange={ this.handleChange} placeholder="Enter your first name" required />
+                    <input type="text" name="lastName" value={this.state.lastName} onChange={ this.handleChange} placeholder="Enter your last name" autoComplete="on" required />
+                    <input type="email" name="emailAddress"  value={this.state.emailAddress} onChange={ this.handleChange} placeholder="Enter your E-mail address." autoComplete="on" required />
+                    <input type="text" name="meterNumber"  value={this.state.meterNumber} onChange={ this.handleChange} max="9" placeholder="Enter your meter number" autoComplete="on" required />
+                    <input type="password" name="passWord" value={this.state.passWord} onChange={ this.handleChange} placeholder="Set a password here" required />
+                    <select id="disco" name="disco" value={this.state.disco} onChange={ this.handleChange}  required>
+                        <option value="null">Select disco</option>
+                        <option value="Eko Electric">Eko Electric</option>
+                        <option value="IE">Ikeja Electric</option>
                     </select>
                     <button type="submit" onClick={this.setRedirect}>Continue</button>
                 </form>
